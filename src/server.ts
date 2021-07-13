f65f077d9d308466b02e70eacd74117b1856d3cf
lmorais <leozonnn@gmail.com>
2021-07-12 21:26:11 -0300

    connection app with database and configuration functional tests

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,17 @@ import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
+import * as database from '@src/database';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupController();
+    await this.databaseSetup();
   }
 
   public getApp(): Application {
@@ -27,6 +29,12 @@ export class SetupServer extends Server {
 
     this.addControllers([forcastController]);
   }
+  
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
 
- 
+  public async close(): Promise<void> {
+    await database.close();
+  }
 }
