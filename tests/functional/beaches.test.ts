521ed1b8b8622473daecaa02f4ef9e93e697b381
lmorais <leozonnn@gmail.com>
2021-07-12 22:14:53 -0300

    implementation logic in controller and create more tests

--- tests/functional/beaches.test.ts
+++ tests/functional/beaches.test.ts
@@ -1,4 +1,11 @@
+import { Beach } from '@src/model/beach';
+
 describe('Beaches functional', () => {
+  beforeAll(async () => {
+    await Beach.deleteMany();
+  })
+
+
   describe ('When create a beach', () => {
     it('should create a beach with success', async () => {
       const newBeach = {
@@ -12,6 +19,28 @@ describe('Beaches functional', () => {
 
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newBeach))
-    })
+    });
+
+
+    it.skip('should return throw 422 when there is a validation error', async () => {
+      const newBeach = {
+        lat: 'invalid_string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error: 
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) path "lat"',
+      })
+    });
+
+    it.skip('should return 500 when there is any error other than validation error', async () => {
+      //TODO think in a way to throw a 500
+    });
   })
 })
